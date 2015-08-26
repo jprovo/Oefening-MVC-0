35c561bf99924a290c45141c5726c9a9d0a29ac9
Jonathan Provo <j.provo@easi.net>
2015-08-26 13:06:40 +0200

    Update.

--- Oefening-MVC-0/Classes/SwitchViewController.h
+++ Oefening-MVC-0/Classes/SwitchViewController.h
@@ -3,7 +3,7 @@
 //  Oefening-MVC-0
 //
 //  Created by Jonathan Provo on 14/08/15.
-//  Copyright (c) 2015 EASI. All rights reserved.
+//  Copyright (c) 2015. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
